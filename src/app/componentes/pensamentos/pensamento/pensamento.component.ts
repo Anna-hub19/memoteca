b2fff2cd91848ad826b621fe0a7d1c6ec6186b2a
anna-hub19 <annabastosbeatriz@gmail.com>
2023-12-13 10:03:38 -0300

    aula 5

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,20 +8,24 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
   
- @Input() pensamento = {
+  @Input() pensamento: Pensamento = {
+    id: 0, // colocamos o valor 0 pois os dados reais do `id` virão do componente "pai".
     conteudo: 'I love Angular',
     autoria: 'Nay',
     modelo: 'modelo3'
 }
+constructor() { }
+
+ngOnInit(): void {
+}
+
 larguraPensamento(): string {
   if(this.pensamento.conteudo.length >= 256) {
     return 'pensamento-g'
   }
   return 'pensamento-p'
 }
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
+
+
